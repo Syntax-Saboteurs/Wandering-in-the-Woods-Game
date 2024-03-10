69983506c23fd489e9c178220aa9f52a3926c928
mjleuck <38877429+mjleuck@users.noreply.github.com>
2024-03-10 16:33:31 -0500

    Update level3_5.py

--- level3_5.py
+++ level3_5.py
@@ -1,20 +1,6 @@
 '''
 resources: https://www.pygame.org/docs/
-
-'''
-# level_two() function - game level grades(3-5)
-    # students can set up the size of their grid, which can now also be rectangular
-    # there can be more than two players as many as four
-    # students can place them where ever they want to on the grid to start
-    # Once a pair has found eachother the pair will then search out another and another assuming more than two players
-    # Once the game is started
-        # it can be played and replayed over and over
-        # the stats of the players meeting can be displayed as:
-            # longest run without meeting
-            # shorted run without meeting
-            # and average run without meeting
-            # assuming this means for all games played  
-        
+'''        
 import pygame, time, sys, random
 from pygame.locals import *
 # Constants
@@ -22,8 +8,8 @@ from pygame.locals import *
 WIDTH   = 600
 HEIGHT  = 600 
 # This sets up the amount of rows and columns in our grid - if we change these values the number of squares changes
-ROWS = 5
-COLS = 5
+ROWS = 10
+COLS = 10
 # this is where we get our size of each cell 
 CELL_SIZE = (WIDTH // COLS, HEIGHT // ROWS)
 
@@ -32,13 +18,14 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 sky_surface = pygame.image.load("graphics/sky.png").convert()
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-#snail_x_pos = 800
-snail_rect = snail_surf.get_rect(midtop = (538,524))
+snail_rect = snail_surf.get_rect(midtop = (555,570))
 
 player_surf = pygame.image.load('graphics/ladyBug.png').convert_alpha()
-player_rect = player_surf.get_rect(midtop = (60,20))
+player_rect = player_surf.get_rect(midtop = (5,4))
 
-
+# create new size 
+snail_surf = pygame.transform.scale(snail_surf, (CELL_SIZE))
+player_surf = pygame.transform.scale(player_surf, (CELL_SIZE))
 
 # color tuples Constants - made of (r, g, b) if all are 255 it is white and if all values are zero then it is black - dont know what we will be using yet
 WHITE = (255, 255, 255)
@@ -48,10 +35,8 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 FOREST_GREEN = (190, 255, 0)
 
-
 # pygame setup - this initializes pygame
 pygame.init()
-
 
 # clock object which will track the game to FPS as seen below
 clock = pygame.time.Clock()
@@ -68,6 +53,65 @@ pygame.display.set_caption("Wandering Woods")
 # set up font (style, size)
 font = pygame.font.SysFont(None, 30)
 
+# sounds -- https://www.sounds-resource.com/
+# background sound - works just add sound file
+cheer = pygame.mixer.Sound('sounds/cheer.wav')
+collision = pygame.mixer.Sound('sounds/collision.wav')
+pygame.mixer.music.load('sounds/space.mp3')
+pygame.mixer.music.play(-1, 0.0)
+
+text_input = ''
+
+# get_input() function -- will get users input for grid 
+def get_input(x):
+    
+    # empty 
+    input_text = ''
+    
+    while True:
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+
+                # Check if the pressed key represents a digit (0-9)
+                if event.unicode.isdigit():
+
+                    # Append the digit to the input_text
+                    input_text += event.unicode  
+
+                elif event.key == pygame.K_RETURN:
+                    
+                    # Convert the input_text to an integer and return it when Enter is pressed
+                    try:
+                        return int(input_text)
+                    except ValueError:
+                        # Handle the case where the input_text cannot be converted to an integer
+                        input_text = ""
+                elif event.key == pygame.K_BACKSPACE:
+                    # Remove the last character if Backspace is pressed
+                    input_text = input_text[:-1]  
+
+        # Clear the screen
+        screen.fill(FOREST_GREEN)
+
+        if x == 2:
+            text_surf_row = font.render("Select a number for the amount of rows: " + input_text, True, BLACK)
+            screen.blit(text_surf_row, (10, 10))
+        elif x == 3:
+            text_surf_cols = font.render("Select a number for the amount of rows: " + input_text, True, BLACK)
+            screen.blit(text_surf_cols, (10, 10))
+        else:
+            # Display the input text
+            text_surface = font.render("Select 1 to start or select 2 to create grid: " + input_text, True, BLACK)
+            screen.blit(text_surface, (10, 10))
+
+        # Update the display
+        pygame.display.update()
+
 # create_text()function - takes five parameters -- what is to be said, in what font, display mode, location on screen
 def create_text(text, font, screen, x, y):
     dialoge = font.render(text, 2, BLACK)
@@ -75,59 +119,50 @@ def create_text(text, font, screen, x, y):
     textBox.topleft = (x, y)
     screen.blit(dialoge, textBox)
 
-def draw_grid():
-    # this creates the cells that correlate to the WIDTH
-    for x in range(0, WIDTH, CELL_SIZE[0]):
-        pygame.draw.line(screen, BLACK, (x, 0), (x, HEIGHT))
-    # this creates the cells that correlate to the HEIGHT
-    for y in range(0, HEIGHT, CELL_SIZE[1]):
-        pygame.draw.line(screen, BLACK, (0, y), (WIDTH, y))
+# students can set up the size of their grid, which can now also be rectangular
+# draw_grid() function -- allows the user to create grid map and size
+def draw_grid(l, r, c):
+    
+    # This sets up the amount of rows and columns in our grid - if we change these values the number of squares changes
+    rows = r
+    cols = c
 
+    # this is where we get our size of each cell 
+    cell_size = (WIDTH // cols, HEIGHT // rows)
 
-# level_one() function - game level for grades(k-2) 
-def level_one():
-    # grids are always square there are always two players starting diagonally from one another
-    # wander out randomly - assuming they control their direction
-    # each move is counted in a counter
-    # music plays as cartoon characters wander in the woods
-    # when the players bump into each other
-        # there is happy graphics displayed
-        # and stats from the wandering are displayed and announced audibly
-        # then the game is reset and the students can start it up again if they would like
-    return
-
-# level_two() function - game level grades(3-5)
-def level_two():
-    # students can set up the size of their grid, which can now also be rectangular
-    # there can be more than two players as many as four
-    # students can place them where ever they want to on the grid to start
-    # Once a pair has found eachother the pair will then search out another and another assuming more than two players
-    # Once the game is started
-        # it can be played and replayed over and over
-        # the stats of the players meeting can be displayed as:
-            # longest run without meeting
-            # shorted run without meeting
-            # and average run without meeting
-            # assuming this means for all games played          
-    return
-
-# level_three() function - game level for grades(6-8)
-def level_three():
-    # students have control of 3-5 game (level II)
-    # students will now be challanged to run experiments to determine how the average run varies with
-        # size and shape of the grids.
-    # the students will also be able to explore differnt protocols for wandering, and to decide which is
-        # the best way to wander if you want to shorten the time it takes to meet up
-    return
-
-
+    # if l (level) == 1 use the standard grid 
+    if l == 1:
+        
+        # this creates the cells that correlate to the WIDTH
+        for x in range(0, WIDTH, CELL_SIZE[0]):
+            pygame.draw.line(screen, BLACK, (x, 0), (x, HEIGHT))
+        # this creates the cells that correlate to the HEIGHT
+        for y in range(0, HEIGHT, CELL_SIZE[1]):
+            pygame.draw.line(screen, BLACK, (0, y), (WIDTH, y))
+            
+    # else use the customizable draw_grid
+    else:
+        # this creates the cells that correlate to the WIDTH
+        for x in range(0, WIDTH, cell_size[0]):
+            pygame.draw.line(screen, BLACK, (x, 0), (x, HEIGHT))
+        # this creates the cells that correlate to the HEIGHT
+        for y in range(0, HEIGHT, cell_size[1]):
+            pygame.draw.line(screen, BLACK, (0, y), (WIDTH, y))
+        
+# main() function
 def main():
     running = True
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    player_turn = 0  # 0 for player 1, 1 for player 2
-    players = [player_rect, snail_rect]  # Assuming there are only two players
-    win_font = pygame.font.SysFont(None, 48)  # Font for "You Win" message
+    #screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("LEVEL: II")
+    player_turn = 0  
+    players = [player_rect, snail_rect]  
+    win_font = pygame.font.SysFont(None, 48)  
 
+    # Call the get_integer_input function to start receiving input
+    lvl = get_input(0)
+    rows = get_input(2)
+    cols = get_input(3)
+    # start game loo[
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -140,7 +175,6 @@ def main():
                 current_player_rect = players[player_turn]
                 current_row = current_player_rect.y // (HEIGHT // ROWS)
                 current_col = current_player_rect.x // (WIDTH // COLS)
-
                 # Check if the clicked position is adjacent to the current player position
                 if (abs(clicked_row - current_row) <= 1 and abs(clicked_col - current_col) <= 1 and
                         (abs(clicked_row - current_row) != 0 or abs(clicked_col - current_col) != 0)):
@@ -148,13 +182,18 @@ def main():
                     current_player_rect.y = clicked_row * (HEIGHT // ROWS)
                     # Switch to the next player's turn
                     player_turn = (player_turn + 1) % len(players)
-
+        
+        # creates the background and objects in the game level
         screen.blit(sky_surface, (0, 0))
         screen.blit(sky_surface, (0, 300))
+        
+        screen.blit(snail_surf, snail_rect)
+        screen.blit(player_surf, player_rect)
         screen.blit(snail_surf, snail_rect)
         screen.blit(player_surf, player_rect)
 
-        draw_grid()
+        # creates user control
+        draw_grid(lvl, rows, cols)
 
         # Check if players overlap
         if player_rect.colliderect(snail_rect):
@@ -162,14 +201,18 @@ def main():
             win_text = win_font.render("You Win!", True, (255, 255, 255))
             win_rect = win_text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
             screen.blit(win_text, win_rect)
-            # Stop the game loop
-            running = False
 
+            #create_text('Total moves it took to collide: ', font, screen,  30, 10)
+            cheer.play(fade_ms=3000)
+
+            # Stop the game loop
+            # running = False
+        
         pygame.display.update()
         clock.tick(60)
-
+        
     pygame.quit()
     sys.exit()
-
+    
 if __name__ == "__main__":
     main()
